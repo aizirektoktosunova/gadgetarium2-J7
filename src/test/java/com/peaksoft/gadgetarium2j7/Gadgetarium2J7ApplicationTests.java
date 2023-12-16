a30ff04cc8ce4b71909d68b34935a55db350fb8c
timurlankasymbaev <moscow.peaksoft@gmail.com>
2023-12-16 18:05:38 +0300

    Initial commit

---
+++ src/test/java/com/peaksoft/gadgetarium2j7/Gadgetarium2J7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peaksoft.gadgetarium2j7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gadgetarium2J7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
