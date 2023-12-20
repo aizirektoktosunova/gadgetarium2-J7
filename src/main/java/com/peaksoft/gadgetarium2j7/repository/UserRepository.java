bedd70125e04aab2c834f6272f9682e768da1f5c
nurte <nurtegingamer@gmail.com>
2023-12-20 16:17:20 +0300

    i deleted some classes

--- src/main/java/com/peaksoft/gadgetarium2j7/repository/UserRepository.java
+++ src/main/java/com/peaksoft/gadgetarium2j7/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.peaksoft.gadgetarium2j7.repository;
 
 import com.peaksoft.gadgetarium2j7.model.entity.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
