9dcace38d652557503b7825c5e665848ed41323c
Бекмамат Боронбай уулу <boronbaibekmamat@gmail.com>
2024-01-04 10:48:05 +0300

    Feature/ sing-up_with_google

--- src/main/java/com/peaksoft/gadgetarium2j7/repositoriy/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.peaksoft.gadgetarium2j7.repositoriy;
-
-import com.peaksoft.gadgetarium2j7.model.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
